afd4eda7b6947afa3136ffc0939d4ea021c4b8d1
AlexanderLegon <alexlegon2014@gmail.com>
2021-07-12 23:12:26 +0100

    completed basic functions

--- dom/events.js
+++ dom/events.js
@@ -32,35 +32,91 @@
 var firstNumber = null;
 var secondNumber = null;
 var operator = null;
+var result = null;
+var zero = "0";
 //if first = null replace, otherwise concat, null is falsey
 
-
-var buttons = document.getElementsByClassName('button');
+var operators = document.getElementsByClassName('operator');
+var numbers = document.getElementsByClassName('numberInput');
+// var buttons = document.getElementsByClassName('button');
 var screen = document.getElementById("screen");
 
 function calculateResult(){
-  console.log(firstNumber + "" + operator + ""+ secondNumber);
-  screen.innerHTML = firstNumber + "" + operator+ ""+secondNumber;
-}
-
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click',function(){
-    // screen.innerHTML = buttons[i].innerHTML;
-    if(!firstNumber){
-      firstNumber = buttons[i].innerHTML;
-      screen.innerHTML = firstNumber;
+  if(secondNumber !== null){
+    console.log(operator);
+    if(operator == "+"){
+        result  = parseInt(firstNumber) + parseInt(secondNumber);
+        screen.innerHTML = result;
     }
-    else if(!operator){
-      operator = buttons[i].innerHTML;
+    if(operator == "-"){
+        result  = parseInt(firstNumber) - parseInt(secondNumber);
+        screen.innerHTML = result;
     }
-    else if(!secondNumber){
-      secondNumber = buttons[i].innerHTML;
-      screen.innerHTML = secondNumber;
+    if(operator == "*"){
+        result  = parseInt(firstNumber) * parseInt(secondNumber);
+        screen.innerHTML = result;
     }
-  })
-
-  if(buttons[i].innerHTML.match("=")){
-    buttons[i].addEventListener('click',calculateResult)
+    if(operator == "/"){
+        result  = parseInt(firstNumber) / parseInt(secondNumber);
+        screen.innerHTML = result;
+    }
+    if(operator == "%"){
+        result  = parseInt(firstNumber) % parseInt(secondNumber);
+        screen.innerHTML = result;
+    }
   }
 }
+
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click',function(){
+    if(!secondNumber && !operator){
+      if(!firstNumber){
+        firstNumber = numbers[i].innerText;
+        screen.innerHTML = firstNumber;
+      }
+      else{
+        firstNumber = firstNumber + numbers[i].innerText;
+        screen.innerHTML = firstNumber;
+      }
+    }else if (!secondNumber) {
+      secondNumber = numbers[i].innerText;
+      screen.innerHTML = secondNumber;
+    }
+    else{
+      secondNumber = secondNumber + numbers[i].innerText;
+      screen.innerHTML = secondNumber;
+    }
+  });
+}
+
+for (let j = 0; j < operators.length; j++) {
+  operators[j].addEventListener('click',function(){
+    if(!operators[j].innerText.match("=")){
+    operator = operators[j].innerText;
+    console.log(operator);
+    if(operators[j].innerText.match("A/C")){
+      screen.innerHTML = zero;
+      operator = null;
+      firstNumber = null;
+      secondNumber = null;
+    }
+    }
+  });
+  if(operators[j].innerText.match("=")){
+    operators[j].addEventListener('click',calculateResult)
+  }}
+
+
+// screen.innerHTML = buttons[i].innerHTML;
+// if(!firstNumber){
+//   firstNumber = buttons[i].innerText;
+//   screen.innerHTML = firstNumber;
+// }
+// else if(!operator){
+//   operator = buttons[i].innerText;
+// }
+// else if(!secondNumber){
+//   secondNumber = buttons[i].innerText;
+//   screen.innerHTML = secondNumber;
+// }
